b0607c4734f1d6fae1fc2223502e2a285a17eb8b
Hir0zuki <s.iremozdemir@gmail.com>
2024-07-05 15:04:17 +0300

    homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function homepage() {
+function Homepage() {
+  const navigate = useNavigate();
+
+  const navigateToRegister = () => {
+    navigate('/register');
+  };
+
   return (
+    // User Auth yapıldığı vakit bu kısım silinecek
     <div>
-       Homepage
+      <div className='userauth'>
+        <div className='upper'>
+          App Name 
+          <br />
+          Didn't you discover the Neverland yet? 
+        </div> 
+        <div className='bottom'>
+          <button type="button" className="btn btn-secondary" onClick={navigateToRegister}>Create a free account</button> or <a href="/login" className="login-link">login</a> if you have an account.
+        </div>   
+      </div>
+        
+      <div className='lists'>
+        <div className='aniMovies'>
+          <div className='aniMovUp'>
+            Movies
+            Database Num
+          </div>
+          <div className='aniMovBottom'>
+            Movies List
+          </div>
+        </div>
+        <div className='aniSeries'>
+          <div className='aniSerUp'>
+            Series
+            Database Num
+          </div>
+          <div className='aniSerBottom'>
+            Series List
+          </div>
+        </div>
+      </div>
+
+      <div className='bottom'>
+        <div className='Right'>
+          <div className='news'>
+            Recently News
+          </div>
+          <div className='forumTop'>
+            Forum Topics
+          </div>
+        </div>
+        <div className='Left'>
+          <div className='lists'>
+            Popular Lists
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default homepage
+export default Homepage;
